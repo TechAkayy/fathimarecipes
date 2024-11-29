2d8badc5db08dba7ec63404c2cb5387a13c62351
Akayy <techakayy@gmail.com>
2024-11-29 16:20:50 +1100

    Added admin redirect page

---
+++ app/pages/admin.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  // will open 'https://nuxt.com' in a new tab
+  await navigateTo('https://admin.fathimarecipes.com/wp-admin/', {
+    external: true,
+    open: {
+      target: '_blank',
+      windowFeatures: {
+        width: 500,
+        height: 500,
+      },
+    },
+  })
+</script>
+
+<template>
+  <div>Redirecting...</div>
+</template>
+
+<style scoped></style>
